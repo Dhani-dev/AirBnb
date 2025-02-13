7468d55914333bfda02649240b4635a89de24121
emanuelongo <emanuel1001cano@gmail.com>
2025-02-13 08:48:49 -0500

    Changes about main with use

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
-import'./signup.js' 
-import './logout.js'
-import './login.js'
 
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-auth.js"
-import { auth } from './firebase.js'
-import { loginCheck } from './loginCheck.js'
-
-onAuthStateChanged(auth, async (user) => {
-    loginCheck(user)
-})
 
 
 (function ($) {
--- js/use.js
+++ js/use.js
@@ -1,5 +1,13 @@
+
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-auth.js"
 import { auth } from './firebase.js'
+import { loginCheck } from './loginCheck.js'
 
-import './signup.js'
+import './signup.js' 
 import './logout.js'
+import './login.js'
+
+
+onAuthStateChanged(auth, async (user) => {
+    loginCheck(user)
+})
