445105eda7a6c85431a4b40a38ffa4326f03f985
emanuelongo <emanuel1001cano@gmail.com>
2025-02-12 20:36:29 -0500

    Changes about things

---
+++ js/logout.js
@@ -0,0 +1,16 @@
+import { signOut } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-auth.js"
+import { auth } from './firebase.js'
+
+const observer = new MutationObserver(() => {
+    const logout = document.querySelector('#logout');
+    if (logout) {
+        console.log('Botón de logout encontrado.');
+        logout.addEventListener('click', async () => {
+            await signOut(auth)
+            console.log('user signed out');
+        });
+        observer.disconnect(); // Dejar de observar cambios en el DOM
+    }
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
--- js/use.js
+++ js/use.js
@@ -1,1 +1,5 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-auth.js"
+import { auth } from './firebase.js'
+
 import './signup.js'
+import './logout.js'
